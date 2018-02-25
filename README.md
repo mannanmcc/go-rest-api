66f1df7444d58f3f6f1d0cdc8d615fe862dab43a
mannanmcc <mannanmcc@gmail.com>
2018-02-25 10:48:24 +0000

    secure add company route with negroni middleware

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Generating the server certificate and private key with OpenSSL takes just one co
 ```
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout key.pem -out cert.pem
 ```
+
+
+Make and point secured with Negroni
+
+test:
+In post man, select Headers
+Then set 'Authorisation' as key and add set token with Bearer [e.g Bearer kgfkljgkldfjgkldjfgkljdfg]
--- Routes.go
+++ Routes.go
@@ -3,15 +3,24 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"github.com/mannanmcc/rest-api/handlers"
+	"github.com/codegangsta/negroni"
+	"net/http"
 )
 
 func NewRouter(env handlers.Env) *mux.Router {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/add-company", env.AddNewCompany).Methods("POST")
-	r.HandleFunc("/update-company", env.UpdateCompany).Methods("POST")
-	r.HandleFunc("/search", env.Search).Methods("GET")
+	r.HandleFunc("/get-token", env.GetToken).Methods("POST")
+	//r.HandleFunc("/company/add", env.AddNewCompany).Methods("POST")
+	r.HandleFunc("/company/update", env.UpdateCompany).Methods("POST")
+	r.HandleFunc("/company/search", env.Search).Methods("GET")
 	r.HandleFunc("/company/{id:[0-9]+}", env.GetCompany).Methods("GET")
+
+	//wrap a route with negroni to make it secure
+	r.Handle("/company/add", negroni.New(
+		negroni.HandlerFunc(env.ValidateTokenMiddleware),
+		negroni.Wrap(http.HandlerFunc(env.AddNewCompany)),
+	))
 
 	return r
 }
